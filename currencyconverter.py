9e6358362f498280f1120822e9e51b97127deec4
shahid531 <76727892+shahid531@users.noreply.github.com>
2020-12-31 18:04:48 +0530

    Add files via upload

--- currencyconverter.py
+++ currencyconverter.py
@@ -1,5 +1,4 @@
 # Simple Currency Converter
-
 while True:
     try:
         countryNameOne = input("Enter Country Name: India,USA or Japan: ")
@@ -14,20 +13,20 @@ while True:
             print(usaUser,'Rupees')
             break
         elif countryNameOne == 'India' and countryNameTwo == 'Japan':
-            usaUser = input1 * 1.41
-            print(usaUser,'Yen')
+            indianUser = input1 * 1.41
+            print(indianUser,'Yen')
             break
         elif countryNameOne == 'Japan' and countryNameTwo == 'India':
-            usaUser = input1 * 0.71
-            print(usaUser,'Rupees')
+            japaneseUser = input1 * 0.71
+            print(japaneseUser,'Rupees')
             break
         elif countryNameOne == 'USA' and countryNameTwo == 'Japan':
             usaUser = input1 * 103.03
             print(usaUser,'Yen')
             break
         elif countryNameOne == 'Japan' and countryNameTwo == 'USA':
-            usaUser = input1 * 0.0097
-            print(usaUser,'USD')
+            japaneseUser = input1 * 0.0097
+            print(japaneseUser,'USD')
             break
         elif countryNameOne == countryNameTwo:
             print('Please enter different country name')
