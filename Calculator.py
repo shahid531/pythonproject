dc519365a2e425457b51011785996c4f4378f830
shahid531 <76727892+shahid531@users.noreply.github.com>
2021-01-10 20:19:41 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+# SIMPLE CALCULATOR USING PYTHON:
+input1 = int(input("Enter the first no: "))
+input3 = input("Choose from operator '+','-','/','*','//','**',: ")
+input2 = int(input("Enter the second no: "))
+if input3 == "+":
+    print(f"Answer: {input1 + input2}")
+elif input3 == "-":
+    print(f"Answer: {input1 - input2}")
+elif input3 == "/":
+    print(f"Answer: {input1 / input2}")
+elif input3 == "*":
+    print(f"Answer: {input1 * input2}")
+elif input3 == "//":
+    print(f"Answer: {input1 // input2}")
+elif input3 == "**":
+    print(f"Answer: {input1 ** input2}")
+print("Thanks For Using...")
+
+
+
+
+
+
+
+
+
+
+
+
+
